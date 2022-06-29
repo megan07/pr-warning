d70218b80030afdecb4a9f30fd3dcad58f3c59d3
Megan Bang <mbang@hashicorp.com>
2022-06-29 13:33:40 -0500

    PR Warning GHA

--- README.md
+++ README.md
@@ -1,2 +1,21 @@
-# pr-warning
-This GitHub action will post a WARNING comment on a PR on `opened`, `reopened` or `ready-for-review`.
+# Post a comment javascript action
+
+This action adds a comment to an open PR
+
+## Inputs
+
+## `repo-token`
+
+**Required** The repo's GITHUB_TOKEN.
+
+## `warning`
+
+**Required** The warning message to comment. The provided message will be prepended with `** WARNING **: `.
+
+## Example usage
+```
+uses: actions/hello-world-javascript-action@v1.5
+with:
+  repo-token: ${{ secrets.GITHUB_TOKEN }}
+  warning: "a warning"
+```
---
+++ dist/index.js
@@ -0,0 +1,147 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	var __webpack_modules__ = ({
+
+/***/ 865:
+/***/ ((module) => {
+
+module.exports = eval("require")("@actions/core");
+
+
+/***/ }),
+
+/***/ 663:
+/***/ ((module) => {
+
+module.exports = eval("require")("@actions/github");
+
+
+/***/ })
+
+/******/ 	});
+/************************************************************************/
+/******/ 	// The module cache
+/******/ 	var __webpack_module_cache__ = {};
+/******/ 	
+/******/ 	// The require function
+/******/ 	function __nccwpck_require__(moduleId) {
+/******/ 		// Check if module is in cache
+/******/ 		var cachedModule = __webpack_module_cache__[moduleId];
+/******/ 		if (cachedModule !== undefined) {
+/******/ 			return cachedModule.exports;
+/******/ 		}
+/******/ 		// Create a new module (and put it into the cache)
+/******/ 		var module = __webpack_module_cache__[moduleId] = {
+/******/ 			// no module.id needed
+/******/ 			// no module.loaded needed
+/******/ 			exports: {}
+/******/ 		};
+/******/ 	
+/******/ 		// Execute the module function
+/******/ 		var threw = true;
+/******/ 		try {
+/******/ 			__webpack_modules__[moduleId](module, module.exports, __nccwpck_require__);
+/******/ 			threw = false;
+/******/ 		} finally {
+/******/ 			if(threw) delete __webpack_module_cache__[moduleId];
+/******/ 		}
+/******/ 	
+/******/ 		// Return the exports of the module
+/******/ 		return module.exports;
+/******/ 	}
+/******/ 	
+/************************************************************************/
+/******/ 	/* webpack/runtime/define property getters */
+/******/ 	(() => {
+/******/ 		// define getter functions for harmony exports
+/******/ 		__nccwpck_require__.d = (exports, definition) => {
+/******/ 			for(var key in definition) {
+/******/ 				if(__nccwpck_require__.o(definition, key) && !__nccwpck_require__.o(exports, key)) {
+/******/ 					Object.defineProperty(exports, key, { enumerable: true, get: definition[key] });
+/******/ 				}
+/******/ 			}
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/hasOwnProperty shorthand */
+/******/ 	(() => {
+/******/ 		__nccwpck_require__.o = (obj, prop) => (Object.prototype.hasOwnProperty.call(obj, prop))
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/make namespace object */
+/******/ 	(() => {
+/******/ 		// define __esModule on exports
+/******/ 		__nccwpck_require__.r = (exports) => {
+/******/ 			if(typeof Symbol !== 'undefined' && Symbol.toStringTag) {
+/******/ 				Object.defineProperty(exports, Symbol.toStringTag, { value: 'Module' });
+/******/ 			}
+/******/ 			Object.defineProperty(exports, '__esModule', { value: true });
+/******/ 		};
+/******/ 	})();
+/******/ 	
+/******/ 	/* webpack/runtime/compat */
+/******/ 	
+/******/ 	if (typeof __nccwpck_require__ !== 'undefined') __nccwpck_require__.ab = __dirname + "/";
+/******/ 	
+/************************************************************************/
+var __webpack_exports__ = {};
+// This entry need to be wrapped in an IIFE because it need to be in strict mode.
+(() => {
+"use strict";
+__nccwpck_require__.r(__webpack_exports__);
+/* harmony export */ __nccwpck_require__.d(__webpack_exports__, {
+/* harmony export */   "run": () => (/* binding */ run)
+/* harmony export */ });
+const core = __nccwpck_require__(865);
+const github = __nccwpck_require__(663);
+
+async function run() {
+  try {
+    const token = core.getInput("repo-token");
+    if (!token) {
+      core.setFailed("Missing token.");
+      return;
+    }
+
+    const warning = core.getInput("warning");
+    const octokit = github.getOctokit(token);
+    const pr = {
+      owner: github.context.issue.owner,
+      repo: github.context.issue.repo,
+      number: github.context.issue.number
+    };
+
+    const prOpenOptions = ['opened', 'reopened', 'ready-for-review']
+    if (!prOpenOptions.includes(github.context.payload.action)) {
+      console.log('No pull request was opened, skipping');
+      return;
+    }
+
+    // Create a comment
+    if (!warning) {
+      core.setFailed("Missing warning.");
+      return;
+    }
+    await octokit.rest.issues.createComment({
+      owner: pr.owner,
+      repo: pr.repo,
+      issue_number: pr.number,
+      body: "** WARNING **: ${warning}",
+    });
+
+    core.info(
+      `Created comment '${warning}' on PR '${pr.number}'.`
+    );
+  } catch (error) {
+    core.setFailed(error.message);
+    if (error.message == 'Resource not accessible by integration') {
+      core.error(`See this action's readme for details about this error`);
+    }
+  }
+}
+
+run();
+})();
+
+module.exports = __webpack_exports__;
+/******/ })()
+;
